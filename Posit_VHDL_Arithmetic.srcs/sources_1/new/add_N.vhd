a1700027d0d147e7cf6234146c7036e145315b7c
Jan Hoertig <janhoertig@gmail.com>
2023-06-28 15:00:22 +0200

    initial commit
    Signed-off-by: Jan Hoertig <janhoertig@gmail.com>

---
+++ Posit_VHDL_Arithmetic.srcs/sources_1/new/add_N.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.06.2023 14:39:13
+-- Design Name: 
+-- Module Name: add_N - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity add_N is
+--  Port ( );
+end add_N;
+
+architecture Behavioral of add_N is
+
+begin
+
+
+end Behavioral;
---
+++ Posit_VHDL_Arithmetic.srcs/sources_1/new/data_extract.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.06.2023 14:39:13
+-- Design Name: 
+-- Module Name: add_N - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity data_extract is
+--  Port ( );
+end data_extract;
+
+architecture Behavioral of data_extract is
+
+begin
+
+
+end Behavioral;
