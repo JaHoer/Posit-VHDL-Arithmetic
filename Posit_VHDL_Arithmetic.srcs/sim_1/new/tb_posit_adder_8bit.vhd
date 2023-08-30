a62cdfcbe61cf3d45252ece3b8448b5a85d6359d
JaHoer <janhoertig@gmail.com>
2023-08-30 14:02:13 +0200

    added Testbanches

---
+++ Posit_VHDL_Arithmetic.srcs/sim_1/new/tb_posit_adder_8bit.vhd
@@ -0,0 +1,98 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Jan Hörig
+-- 
+-- Create Date: 08/30/2023 12:33:32 PM
+-- Design Name: 
+-- Module Name: tb_posit_adder_8bit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_posit_adder_8bit is
+    generic (
+        N_tb : integer := 8;
+        Bs_tb : integer := 3; -- log2(N)
+        es_tb : integer := 4
+        
+    );
+
+--  Port ( );
+end tb_posit_adder_8bit;
+
+
+
+architecture Behavioral of tb_posit_adder_8bit is
+
+    constant CLOCK_PERIOD : time := 100 ns;
+
+    signal clk  : std_logic;
+
+    signal in1_tb : std_logic_vector(N_tb-1 downto 0);
+    signal in2_tb : std_logic_vector(N_tb-1 downto 0);
+    signal start_tb : std_logic;
+    signal out_val_tb : std_logic_vector(N_tb-1 downto 0);
+    signal inf_tb : std_logic;
+    signal zero_tb : std_logic;
+    signal done_tb : std_logic;
+
+begin
+
+    UUT : entity work.posit_adder
+    generic map (
+        N => N_tb,
+        Bs => Bs_tb,
+        es => es_tb
+    )
+    port map (
+        in1 => in1_tb,
+        in2 => in2_tb,
+        start => start_tb,
+        out_val => out_val_tb,
+        inf => inf_tb,
+        zero => zero_tb,
+        done => done_tb
+       
+    );
+    
+    
+    stimuli: process
+    
+    begin
+    
+        wait for CLOCK_PERIOD;
+        -- Zeile 5
+        in1_tb <= "00000100";
+        in2_tb <= "00000000";
+        start_tb <= '1';
+        wait for CLOCK_PERIOD;
+        assert done_tb = '1' report "Should be 1";
+        assert out_val_tb = x"00000100" report "Should be 00000100";
+        wait for CLOCK_PERIOD;
+    
+    end process;
+
+
+end Behavioral;
